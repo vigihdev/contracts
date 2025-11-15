fdfa9d217b74ebdcab97330a2249a524c4ad9c92
vigih dev <vigihdev@gmail.com>
2025-11-16 02:51:25 +0700

    💥 15-11-2025 13-29

---
+++ src/Vehicle/VehicleMultiHargaInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vigihdev\Contracts\Vehicle;
+
+interface VehicleMultiHargaInterface extends VehicleCompactInterface
+{
+
+    /**
+     *
+     * @return VehicleCompactHargaInterface[]|VehicleNotAvailableHargaInterface[]
+     */
+    public function getPaketHargas(): array;
+}
